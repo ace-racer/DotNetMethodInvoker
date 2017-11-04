ec69e4fac2f376acf9e8f828675bf1376a66dbd2
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-11-04 21:07:48 +0530

    Initial code structure commit

---
+++ GenericDotNetMethodInvoker.BusinessLogics/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDotNetMethodInvoker.BusinessLogics
+{
+    public class Class1
+    {
+    }
+}
---
+++ GenericDotNetMethodInvoker.Models/BasicMethodDetails.cs
@@ -0,0 +1,46 @@
+﻿namespace GenericDotNetMethodInvoker.Models
+{
+    public class BasicMethodDetails
+    {
+        /// <summary>
+        /// Gets or sets the assembly location.
+        /// </summary>
+        /// <value>
+        /// The assembly location.
+        /// </value>
+        public string AssemblyLocation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the assembly.
+        /// </summary>
+        /// <value>
+        /// The assembly.
+        /// </value>
+        public string Assembly { get; set; }
+
+        /// <summary>
+        /// Gets or sets the namespace.
+        /// </summary>
+        /// <value>
+        /// The namespace.
+        /// </value>
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the class.
+        /// </summary>
+        /// <value>
+        /// The name of the class.
+        /// </value>
+        public string ClassName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the method.
+        /// </summary>
+        /// <value>
+        /// The method.
+        /// </value>
+        public string Method { get; set; }
+        
+    }
+}
